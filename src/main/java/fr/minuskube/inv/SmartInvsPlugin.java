1dbab5cf49d1c3dcf22eec3ff01f89cf68ef9d50
Timo-Ens <tomencherry10@gmail.com>
2022-01-10 17:50:06 +0100

    Initial

--- src/main/java/fr/minuskube/inv/SmartInvsPlugin.java
+++ src/main/java/fr/minuskube/inv/SmartInvsPlugin.java
@@ -1,21 +1,34 @@
 package fr.minuskube.inv;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import net.minestom.server.extensions.Extension;
 
-public class SmartInvsPlugin extends JavaPlugin {
+public class SmartInvsPlugin extends Extension {
 
     private static SmartInvsPlugin instance;
     private static InventoryManager invManager;
 
+    public static InventoryManager getInvManager() {
+        return invManager;
+    }
+
+    public static InventoryManager manager() {
+        return invManager;
+    }
+
+    public static SmartInvsPlugin instance() {
+        return instance;
+    }
+
     @Override
-    public void onEnable() {
+    public void initialize() {
         instance = this;
 
         invManager = new InventoryManager(this);
         invManager.init();
     }
 
-    public static InventoryManager manager() { return invManager; }
-    public static SmartInvsPlugin instance() { return instance; }
+    @Override
+    public void terminate() {
 
+    }
 }
